e9e3e3eac8683f83dbee67a1b088cb7e1f6dae40
Daniel Henrik Nevermann <74931903+ISCOTTYI@users.noreply.github.com>
2021-03-14 21:24:16 +0100

    added setup.sh draft

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# === HOW TO EXECUTE === 
+# first make sure user got write permission
+#   $ chmod +x script-name-here.sh
+# then run via
+#   $ ./script-name-here.sh
+# or
+#   $ sh script-name-here.sh
+
+echo "execution was successfull"
